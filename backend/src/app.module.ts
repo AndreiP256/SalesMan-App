4600110a60d2df56f6e58bd460cf876702f60c87
AndreiP256 <andreiprusacov@gmail.com>
2024-02-23 11:08:41 +0200

    Changed App modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VisitModule } from './visit/visit.module';
+import { DriverModule } from './driver/driver.module';
+import { ManagerModule } from './manager/manager.module';
+import { UserModule } from './user/user.module';
+import { LoginModule } from './login/login.module';
+import { ClientModule } from './client/client.module';
+import { SalesAgentModule } from './sales-agent/sales-agent.module';
 
 @Module({
-  imports: [],
+  imports: [VisitModule, DriverModule, ManagerModule, UserModule, LoginModule, ClientModule, SalesAgentModule],
   controllers: [AppController],
   providers: [AppService],
 })
